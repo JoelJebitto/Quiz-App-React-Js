9eaa65584fecfcf63652eb3caeec6561cd509720
Joel Jebitto <joeljebitto@gmail.com>
2025-05-22 09:32:27 +0530

    added header and body components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Body from "./components/Body";
+import Header from "./components/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-linear-to-r from-green-300 to-cyan-400 h-screen ">
+      <Header />
+      <Body />
+    </div>
+  );
 }
 
 export default App;
